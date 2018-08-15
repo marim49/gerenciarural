e7863a3da9fb5fc659a62dae447c2cf116796965
Leandro Marim <leandro.marim@hotmail.com>
2018-08-15 17:06:36 -0300

    Adicionado modal cancelar operação e view de reparo de maquina

--- public/js/select/RevisaoMaquina.js
+++ public/js/select/RevisaoMaquina.js
@@ -3,13 +3,34 @@ function Maquinas() {
     fazenda = JSON.parse($("#fazendas").find(':selected').attr('data-id'));
     console.log(fazenda);
     funcionarios = fazenda.funcionarios;
+    maquinas = fazenda.maquinas;
 
     var selectFuncionario = document.getElementById("funcionario");
+    var selectMaquinas = document.getElementById("maquina");
 
     //Limpar Selects
     for (var i in selectFuncionario.options) {
         selectFuncionario.remove(i);
         selectFuncionario.options[selectFuncionario.sel]
+    }
+    for (var i in selectMaquinas.options) {
+        selectMaquinas.remove(i);
+    }
+
+    //Preencher máquinas
+    if (!$.isEmptyObject(maquinas)) {
+        for (var i in maquinas) {
+            var el = document.createElement("option");
+            el.textContent = maquinas[i].nome;
+            el.value = maquinas[i].id;
+            selectMaquinas.appendChild(el);
+        }
+
+    } else {
+        var el = document.createElement("option");
+        el.textContent = 'Vazio';
+        el.value = '';
+        selectMaquinas.appendChild(el);
     }
 
     //Preencher funcionários
--- resources/views/relatorio/raplicacao.blade.php
+++ resources/views/relatorio/raplicacao.blade.php
@@ -16,7 +16,7 @@
 				<div class="panel-body">
 					<div class="table-responsive">
 						
-						<table class="table table-striped table-bordered table-hover" id="aplicacao">
+						<table class="table table-striped table-bordered table-hover" id="daplicacao">
 							<thead>
 								<tr>
 									<th>Funcionário</th>
@@ -25,6 +25,7 @@
 									<th>Quantidade Aplicada</th>
 									<th>Motivo</th>
 									<th>Data da aplicação</th>
+									<th>Cancelar operação</th>
 
 
 								</tr>
@@ -38,19 +39,49 @@
 									<td> {{$historico->quantidade}} </td>
 									<td> {{$historico->motivo}} </td>
 									<td> {{$historico->data}} </td>
+									<td>
+					<center>
+						<button type="button" class="btn btn-xs btn-danger" onclick="" data-toggle="modal"
+						data-target="#modal_theme_danger">Cancelar</button>
+					</center>
+				</td>
 
 								</tr>
 								@endforeach
 								<tfoot>
             <tr>
                 <th>Total:</th>
-                <th colspan="4" style="text-align:center"></th>
+                <th colspan="6" style="text-align:center"></th>
             </tr>
         </tfoot>
 
 							</tbody>
 
 						</table>
+
+						<!-- Modal de delete -->
+						<div id="modal_theme_danger" class="modal fade">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header bg-danger">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h6 class="modal-title">AVISO!</h6>
+              </div>
+
+              <div class="modal-body">
+                <h6 class="text-semibold">Tem certeza que deseja cancelar esta operação?</h6>
+
+              </div>
+
+              <div class="modal-footer">
+                <button type="button" class="btn btn-link" data-dismiss="modal">Não</button>
+                <button type="button" class="btn btn-danger">Sim</button>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Modal de delete -->
 						<a onclick="imprimir()" class="btn btn-primary pull-left">Imprimir</a>
 					</div>
 				</div>
